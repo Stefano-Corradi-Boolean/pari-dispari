b7306dcf837544c61b4a56aa301ba62b095e2ee8
ugodeughi <stefano.corradi@gmail.com>
2024-02-27 10:56:32 +0100

    controllo con while

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
 
-const pariDispari = 'pari'//prompt('Scrivi "pari" o  "dispari"') // prompt
-const numGiocatore = 3//parseInt(prompt('Gioca un numero fra 1 e 5')); // prompt
+let pariDispari;
+let numGiocatore;
+
+let validPariDispari = false;
+let validNumber = false;
+
+// fino a quando non è valido l'iserimento di pari/dispari ripropongo il prompt
+while (!validPariDispari) {
+  pariDispari = prompt('Scrivi "pari" oppure "dispari"')
+  // verifico che la stringa inserita sia 'pari' o 'dispari'
+  if(pariDispari.toLowerCase() === 'pari' || pariDispari.toLowerCase() === 'dispari'){
+    // se la stringa è valita interrompo il ciclo quindi non chiedo un altrpo inserimento
+    validPariDispari = true;
+  }else{
+    alert('Attenzione! Devi scrivere la parola "pari" o "dispari"')
+  }
+}
+
+while(!validNumber){
+  numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
+  if(isNaN(numGiocatore) === false){
+    if(numGiocatore > 0 && numGiocatore <=5){
+      validNumber = true;
+    }else{
+      alert('Attenzione! hai inserito un numero sbagliato')
+    }
+  }else{
+    alert('Attenzione! hai inserito una stringa e non un numero')
+  }
+}
+
+
+
+
 const numPc = Math.ceil(Math.random() * 5)
 console.log(numPc)
 const somma = numPc + numGiocatore;
